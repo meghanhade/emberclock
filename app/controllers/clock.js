772ae274285938ff6872240810adb93d44f58481
Meghan <mmhade@gmail.com>
2015-08-04 18:36:29 -0700

    adding local forage for data source, missing step around editing brocfile.js broke app

--- app/controllers/clock.js
+++ app/controllers/clock.js
@@ -1,20 +1,21 @@
 import Ember from 'ember';
 
 export default Ember.Controller.extend({
-	init: function() {
-		// update the time
-		this.updateTime();
-	},
+    updateTime: function() {
+        var _this = this;
+ 
+        // Update the time every second.
+        Ember.run.later(function() {
+            _this.set('localTime', moment().format('h:mm:ss a'));
+ 
+            _this.get('model').forEach(function(model) {
+                model.set('time',
+                          moment().tz(model.get('name')).format('h:mm:ss a'));
+            });
+ 
+            _this.updateTime();
+        }, 1000);
+    }.on('init'),
 
-	updateTime: function(){
-		var _this = this;
-
-		// update the time every second
-		Ember.run.later(function(){
-			_this.set('localTime', new Date().toLocaleTimeString());
-			_this.updateTime();
-		}, 1000);
-	},
-
-	localTime: new Date().toLocaleTimeString()
+   localTime: moment().format('h:mm:ss a')
 });
